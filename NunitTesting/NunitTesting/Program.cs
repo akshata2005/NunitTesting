f360b34a55b8cabc9c54ef329f3b470aa04092b8
akshata2005 <akshatasawant1805@gmail.com>
2022-01-26 21:24:34 +0530

    [akshata] Added Vending machine

---
+++ NunitTesting/NunitTesting/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitTesting
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("N Unit Testing Programs.......");
+            /*.........................................
+             * Create a switch method to choose a particular operation 
+             * ............................................*/
+            Console.WriteLine("Enter an Option :");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Calculating Minimum number of notes :");
+                    VendingMachine.FindingMinimumNotes();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ NunitTesting/NunitTesting/VendingMachine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitTesting
+{
+    class VendingMachine
+    {
+        public static void FindingMinimumNotes()
+        {
+            Console.WriteLine("Enter a value to return Notes :");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            int[] notes = { 1000, 500, 100, 50, 10, 5, 2, 1 };
+            int nCount = 0;
+
+            int[] notesCount = new int[notes.Length];
+
+            for (int i = 0; i < notes.Length - 1; i++)
+            {
+                if (amount >= notes[i])
+                {
+                    notesCount[i] = amount / notes[i];
+                    amount -= (notesCount[i] * notes[i]);
+                    nCount++;
+                }
+
+            }
+            Console.WriteLine("Total Number of notes required are : " + notesCount.Length);
+            Console.WriteLine("The Notes issued are :");
+
+            for (int i = 0; i < notes.Length - 1; i++)
+            {
+                if (notesCount[i] != 0)
+                {
+                    Console.WriteLine($"{notesCount[i]} : Rs.{notes[i] }required");
+                }
+            }
+        }
+    }
+}
